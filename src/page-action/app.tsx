6195f6125bc3ea4a2099abbbd7c851f60ce45cff
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2021-09-22 21:21:37 +0900

    refactoring

--- src/page-action/app.tsx
+++ src/page-action/app.tsx
@@ -1,10 +1,9 @@
 import _ from "lodash"
-import { DateTime } from "luxon"
 import React, { useCallback, useEffect, useState } from "react"
 import { createUseStyles } from "react-jss"
 import { EVENT_NAVIGATE, EVENT_REQUEST_HISTORY, EVENT_RESPONSE_HISTORY } from "../events"
 import { History } from "../store"
-import classNames from "classnames"
+import { HistoryView } from "./history"
 
 
 const useStyles = createUseStyles( {
@@ -17,47 +16,9 @@ const useStyles = createUseStyles( {
   messageOnly: {
     "margin": "1em",
     "font-size": "15px"
-  },
-  cardList: {
-    "overflow-x": "hidden"
-  },
-  card: {
-    "width": 480,
-    "margin-bottom": "2em",
-    "cursor": "pointer"
-  },
-  cardImage: {
-    "display": "block",
-    "width": 480,
-    "height": 270,
-    "object-fit": "cover"
-  },
-  cardMessage: {
-    "display": "block",
-    "margin-top": "0.4em"
-  },
-  cardFavicon: {
-    "width": 16,
-    "height": 16,
-    "object-fit": "cover",
-    "margin-right": "0.2em",
-    "vertical-align": "middle"
-  },
-  cardMessageBase: {
-    "margin": 0,
-    "padding": "0 1em",
-    "white-space": "nowrap",
-    "text-overflow": "ellipsis",
-    "overflow": "hidden"
-  },
-  cardMessageTitle: {
-    "font-size": "17px",
-    "font-weight": 600
-  },
-  cardMessageBody: {
-    "font-size": "15px"
   }
 } )
+
 
 export function App(): React.ReactElement {
 
@@ -70,11 +31,7 @@ export function App(): React.ReactElement {
           if ( message.event === EVENT_RESPONSE_HISTORY ) {
             const histories = message.history as History[]
             console.debug( histories )
-            setHistories( _.chain( histories )
-                           .dropRight( 1 )  // Removes current page
-                           .sortBy( history => history.timestamp )
-                           .reverse()
-                           .value() )
+            setHistories( histories )
 
           } else {
             console.debug( message )
@@ -84,49 +41,28 @@ export function App(): React.ReactElement {
       }, []
   )
 
-  const onClick = useCallback( ( url: string ) => {
+  const navigate = useCallback( ( url: string ) => {
     browser.runtime.sendMessage( { event: EVENT_NAVIGATE, url } ).then( () => {
       window.close()
     } )
   }, [] )
 
-  const onAuxClick = useCallback( ( e: React.MouseEvent, url: string ) => {
+  const navigateWithNewTab = useCallback( ( e: React.MouseEvent, url: string ) => {
     if ( e.button === 1 ) { // Middle button clicked
       browser.runtime.sendMessage( { event: EVENT_NAVIGATE, url, newTab: true } ).then()
     }
   }, [] )
 
-  return (
-      <div>
-        {histories === undefined
-            ? <p className={classes.messageOnly}>Loading...</p>
-            : _.isEmpty( histories )
-                ? <p className={classes.messageOnly}>The history is empty</p>
-                : (
-                    <div className={classes.cardList}>
-                      {histories.map( history => (
-                          <div key={history.timestamp}
-                               className={classes.card}
-                               onClick={() => onClick( history.tab.url )}
-                               onAuxClick={e => onAuxClick( e, history.tab.url )}>
-                            <img src={history.objectUrl}
-                                 className={classes.cardImage}
-                                 alt="tab thumbnail" />
-                            <div className={classes.cardMessage}>
-                              <p className={classNames( classes.cardMessageBase, classes.cardMessageTitle )}>
-                                <img src={history.faviconUrl} alt="" className={classes.cardFavicon} />
-                                {history.tab.title}
-                              </p>
-                              <p className={classNames( classes.cardMessageBase, classes.cardMessageBody )}>
-                                {history.tab.url}<br />
-                                {DateTime.fromMillis( history.timestamp ).toLocaleString( DateTime.DATETIME_MED_WITH_SECONDS )}
-                              </p>
-                            </div>
-                          </div>
-                      ) )}
-                    </div>
-                )
-        }
-      </div>
-  )
+
+  if ( histories === undefined ) {
+    return <p className={classes.messageOnly}>Loading...</p>
+  } else if ( _.isEmpty( histories ) ) {
+    return <p className={classes.messageOnly}>The history is empty</p>
+  } else {
+    return (
+        <div>
+          <HistoryView histories={histories} onClick={navigate} onAuxClick={navigateWithNewTab} />
+        </div>
+    )
+  }
 }
